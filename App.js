8ed6c665fc133dc3325a7b56cfa81a5c8882917e
AHMED <eng.computer1@yahoo.com>
2021-04-09 23:05:14 -0500

    added some movie photo and name

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios'
+import React,{useEffect} from 'react'
+import data from './src/data'
+import {Image}   from 'react-native'
 
 export default function App() {
+
+
+
+
+
+
+const movies=()=>{
+       
+       console.log(data)
+
+      return  data.movie.map((movie)=>
+         {
+         return   <div>
+         
+           <img src={movie.url} style={{width:"200px",height:"200px"}} />
+            <h2>{movie.name}</h2>
+           </div>
+         }
+       )
+
+
+}
+
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello There, this is my first page for learning react native!</Text>
       <StatusBar style="auto" />
+      {movies()}
     </View>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,17 @@
+const data={
+  movie: [{name:"1917",
+    url:require('../images/1.jpeg')}, 
+    {name:"CAGED",
+   url:require('../images/moviephotos:arge_3KNiO5m9LbhyaOhBY61RP9suYUl.jpeg')}, 
+    {name:"CRISIS",
+   url:require('../images/moviephotos:Crisis_poster.jpeg')}, 
+    {name:"FLIGHT",
+   url:require("../images/moviephotos:Flight-306x329.jpeg")}, 
+    {name:"4K",
+   url:require("../images/moviephotos:maxresdefault.jpeg")},
+   {name:"WRONG TURN",
+   url:require("../images/moviephotos:MV5BM2Y5ZWE2MTMtODE3ZC00NWQ4LWJkNzctNGY4Njg5NDY5MzNlXkEyXkFqcGdeQXVyNjUxMjc1OTM@._V1_.jpg")}
+   ]
+}
+
+export default data
